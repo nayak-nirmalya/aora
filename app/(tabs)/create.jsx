3402a911ea12c499c09fc9f0fe0b5ce705420ecd
Nirmalya Nayak <n.nayak263@gmail.com>
2024-04-17 13:35:10 +0530

    add create page video title input field with state

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,44 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { router } from "expo-router";
+import { ResizeMode, Video } from "expo-av";
+import {
+  View,
+  Text,
+  Alert,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { icons } from "../../constants";
+import { CustomButton, FormField } from "../../components";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Create = () => {
+  const { user } = useGlobalContext();
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
+
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+
+        <FormField
+          title="Video Title"
+          value={form.title}
+          placeholder="Give your video a catchy title..."
+          handleChangeText={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
